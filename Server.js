d84ed59bcdd73c9ee0e8d4b4fdac402e4962f410
Anjali Suman <anjalisuman3011@gmail.com>
2024-04-02 19:14:36 +0530

    get req done

--- Server.js
+++ Server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const Product = require('./Models/productsScehma');
+
+app.use(express.json());
 
 
 
@@ -8,9 +11,18 @@ app.get("/" ,(req,res)=>{
     res.send("hello from home");
 })
 
-// app.post("/data" , (req,res){
+ app.post("/product" , async(req,res)=>{
+   try{
+  const product = await Product.create(req.body)
+  res.status(200).json(product);
+   }
+    catch(error){
+        console.log(error.message)
+        res.status(500)
+
+    }
     
-// })
+ })
 
     mongoose
     .connect('mongodb+srv://Anjali:9905949991@cluster0.yp4wprj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
