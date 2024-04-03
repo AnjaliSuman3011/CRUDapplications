316fd3378da3fa0a26a87e09b59ceb031dbbc0ea
Anjali Suman <anjalisuman3011@gmail.com>
2024-04-03 18:54:13 +0530

    put and delete done

--- Server.js
+++ Server.js
@@ -11,7 +11,65 @@ app.get("/" ,(req,res)=>{
     res.send("hello from home");
 })
 
- app.post("/product" , async(req,res)=>{
+app.get("/products" ,async(req,res)=>{
+    try{
+  const product = await Product.find({});
+  res.status(200).json(product);
+    }
+    catch(error)
+    {
+        console.log(error);
+        res.status(500).json({message:error.message});
+    }
+})
+
+
+app.get("/products/id" ,async(req,res)=>{
+    try{
+        const id = req.params;
+  const product = await Product.findById({id});
+  res.status(200).json(product);
+    }
+    catch(error)
+    {
+        console.log(error);
+        res.status(500).json({message:error.message});
+    }
+})
+
+app.put("/products/:id" ,async(req,res)=>{
+    try{
+        const {id} = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body);
+  if(!product){
+    return res.status(404).json({message:`cannot find any product with {id}`})
+  }
+  res.status(200).json(product);
+    }
+    catch(error)
+    {
+        console.log(error);
+        res.status(500).json({message:error.message});
+    }
+})
+
+app.delete("/products/:id" ,async(req,res)=>{
+    try{
+        const {id} = req.params;
+  const product = await Product.findByIdAndDelete(id);
+  if(!product){
+    return res.status(404).json({message:`cannot find any product with ${id}`});
+  }
+  res.status(200).json(product);
+    }
+    catch(error)
+    {
+        console.log(error);
+        res.status(500).json({message:error.message});
+    }
+})
+
+ app.post("/products" , async(req,res)=>{
    try{
   const product = await Product.create(req.body)
   res.status(200).json(product);
