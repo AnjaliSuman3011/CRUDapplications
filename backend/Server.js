507d64c3a988677ccd7b8f5398016e74b12ec63e
Anjali Suman <anjalisuman3011@gmail.com>
2024-04-10 16:21:45 +0530

    Cors policy

--- backend/Server.js
+++ backend/Server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 const ProductRoute = require('./Routes/ProductRoute');
 app.use(express.json());
